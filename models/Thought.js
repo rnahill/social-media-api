c86883af374a575eb8a125afa004e044c07bccc4
rnahill <rei333lly@gmail.com>
2024-02-05 14:38:10 -0500

    started thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,28 @@
 const { Schema, Types } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.not,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reactions:[
+            //array of nested documents within the reactionSchema
+        ]
+    }
+);
+
+module.exports = thoughtSchema;
