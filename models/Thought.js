9251f1fd0c609e756f60e4a2e017464cede59095
rnahill <rei333lly@gmail.com>
2024-02-05 14:41:31 -0500

    started reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -11,7 +11,7 @@ const thoughtSchema = new Schema(
 
         createdAt: {
             type: Date,
-            default: Date.not,
+            default: Date.now,
         },
 
         username: {
--- models/User.js
+++ models/User.js
@@ -2,10 +2,6 @@ const { Schema, Types } = require('mongoose');
 
 const userSchema = new Schema(
     {
-        userId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-          },
 
         username: {
             type: String,
