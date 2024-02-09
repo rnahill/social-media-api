309d4e89ad1022af43e32267ff5647d98892ce11
rnahill <rei333lly@gmail.com>
2024-02-09 18:53:24 -0500

    started data

---
+++ utils/data.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+        username: "helloworld",
+        email: "hello@world.com",
+        thoughts: [],
+        friends: []
+    },
+
+    {
+        username: "cutefrog",
+        email: "cutefrog@gmail.com",
+        thoughts: [],
+        friends: []
+    },
+
+    {
+        username: "blackberry",
+        email: "fruit@gmail.com",
+        thoughts: [],
+        friends: []
+    },
+];
