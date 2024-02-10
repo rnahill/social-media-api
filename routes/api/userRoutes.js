3f28d5671eb169e54257f9b84c4387033ae44e83
rnahill <rei333lly@gmail.com>
2024-02-10 18:12:08 -0500

    started user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,42 @@
 const router = require('express').Router();
+const { User } = require('../../models/User')
+
+
+// Get all users
+const getUsers = async (req, res) => {
+    try{
+        const users = await User.find().populate('users');
+        res.json(users);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+// Get one user
+
+const getOneUser = async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.params.userId}).populate('users');
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+// Create new user
+const createUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.json(user)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+router.route('/')
+    .get(getUsers)
+    .post(createUser)
+
+router.route('/:userId')
+    .get(getOneUser)
